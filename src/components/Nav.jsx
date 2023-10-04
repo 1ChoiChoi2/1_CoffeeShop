06f9b53571e764fac5a0dee49c380b72700c98fa
1ChoiChoi2 <khanhduc.nguyenhoang12@gmail.com>
2023-10-04 09:42:39 +0700

    Add icon cart on navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Nav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Nav = () => {
   return (
     <nav>
       <div className="nav__container">
-          <Link to="/" className="nav__logo">
-            Coffee
-            <small>Blend</small>
-          </Link>
+        <Link to="/" className="nav__logo">
+          Coffee
+          <small>Blend</small>
+        </Link>
         <ul className="nav__links">
-            <li><Link to="/" className="nav__link">Home</Link></li>
-            <li><Link to="/menu" className="nav__link">Menu</Link></li>
-            <li><Link to="/blog" className="nav__link">Blog</Link></li>
-            <li><Link to="/contact" className="nav__link">Contact</Link></li>
-            <li><Link to="/cart" className="nav__link">Cart</Link></li>
+          <li>
+            <Link to="/" className="nav__link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu" className="nav__link">
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link to="/blog" className="nav__link">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="nav__link">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart" className="nav__link nav__icon">
+              <FontAwesomeIcon icon="shopping-cart"/>
+              <span className="cart__length">2</span>
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faShoppingCart);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
