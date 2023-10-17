9d796a8162e262f1304f32b0ed6f752a7d678ef9
1ChoiChoi2 <khanhduc.nguyenhoang12@gmail.com>
2023-10-17 11:36:49 +0700

    Working on Cart Section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { dishes, blogs } from "./services/data";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
@@ -10,8 +10,32 @@ import Cart from "./pages/Cart";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 
-
 function App() {
+  const [cart, setCart] = useState([]);
+
+  // Add Item to Cart + With Quanity
+  function addToCart(dish, dishQuantity) {
+    const existingDish = cart.find((cartItem) => cartItem.id === dish.id);
+
+    if (dishQuantity > 0) {
+      if (existingDish) {
+        existingDish.quantity += dishQuantity;
+        setCart([...cart]);
+      } else {
+        setCart([...cart, { ...dish, quantity: dishQuantity }]);
+      }
+    } else {
+      setCart([...cart]);
+    }
+  }
+
+  // Remove Item from Cart
+  function removeFromCart (itemId) {
+    const updateCart = cart.filter(cartItem => cartItem.id !== itemId);
+    setCart(updateCart);
+  };
+
+  console.log(cart);
 
   return (
     <div className="App">
@@ -19,10 +43,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu dishes={dishes} />} />
-        <Route path="/menu/:id" element={<DishDetail dishes={dishes}/>} />
+        <Route
+          path="/menu/:id"
+          element={<DishDetail addToCart={addToCart} dishes={dishes} />}
+        />
         <Route path="/blog" element={<Blogs blogs={blogs} />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart}/>} />
       </Routes>
       <Footer />
     </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React from "react";
+import cartBackground from "../assets/cartBackground.jpg";
+import Landing from "../components/Landing";
+import "../styles/Cart.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Cart = () => {
+const Cart = ({ cart, removeFromCart }) => {
   return (
-    <div>Cart</div>
-  )
-}
+    <>
+      <Landing backgroundUrl={cartBackground} subtitle="Cart" />
+      <div id="cart">
+        <div className="container">
+          <div className="row">
+            <div className="cart__container">
+              <div className="cart__product">Product</div>
+              <div className="cart__price">Price</div>
+              <div className="cart__quantity">Quantity</div>
+              <div className="cart__total">Total</div>
+            </div>
+            <div className="cart__items__container">
+              {cart.map((cartItem) => (
+                <div key={cartItem.id} className="cart__item">
+                  <div className="cart__product">
+                    <div
+                      onClick={() => removeFromCart(cartItem.id)}
+                      className="cart__item--remove"
+                    >
+                      <FontAwesomeIcon icon="fa-times" />
+                    </div>
+                    <figure className="cart__item--img">
+                      <img src={cartItem.url} alt="product" />
+                    </figure>
+                    <div className="cart__item--description">
+                      <h3 className="cart__item--title">{cartItem.title}</h3>
+                      <p>{cartItem.description}</p>
+                    </div>
+                  </div>
+                  <div className="cart__price">
+                    ${cartItem.price.toFixed(2)}
+                  </div>
+                  <div className="cart__quantity">
+                    <input
+                      className="cart__item--quantity"
+                      type="number"
+                      defaultValue={cartItem.quantity}
+                      min={0}
+                      max={99}
+                    />
+                  </div>
+                  <div className="cart__total">
+                    ${(cartItem.price * cartItem.quantity).toFixed(2)}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div id="total">
+              <div className="total__container">
+                <h3 className="total__title">Cart Totals</h3>
+                <div className="subtotal">
+                  <span>Subtotal</span>
+                  <span>$10.00</span>
+                </div>
+                <div className="delivery">
+                  <span>Delivery</span>
+                  <span>$3.00</span>
+                </div>
+                <div className="discount">
+                  <span>Discount</span>
+                  <span>$1.00</span>
+                </div>
+                <hr />
+                <div className="total">
+                  <span>Total</span>
+                  <span>$18.00</span>
+                </div>
+              </div>
+              <button className="total__button">
+                Proceed to Checkout
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Cart
+export default Cart;
